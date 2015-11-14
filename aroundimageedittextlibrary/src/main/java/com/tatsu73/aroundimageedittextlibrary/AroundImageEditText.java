b7922334ae5167613a4c877d152ffe130ea3b017
tatsu73 <tatsuro.saito73@gmail.com>
2015-11-14 15:57:53 +0900

    change attr file

--- aroundimageedittextlibrary/src/main/java/com/tatsu73/aroundimageedittextlibrary/AroundImageEditText.java
+++ aroundimageedittextlibrary/src/main/java/com/tatsu73/aroundimageedittextlibrary/AroundImageEditText.java
@@ -5,11 +5,9 @@ import android.animation.PropertyValuesHolder;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
-import android.graphics.Rect;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,8 +25,9 @@ public class AroundImageEditText extends FrameLayout{
     private Context tContext;
     private EditText tEditText;
     private ImageView tImageView;
-    private int firstColor;
-    private int secondColor;
+    private Boolean condition = false;
+    private int firstBackGroundColor;
+    private int secondBackgroundColor;
     private int firstImage;
     private int secondImage;
     private static final int DEFAULT_FIRST_BACKGROUND_COLOR = Color.argb(255, 255, 0, 0);
@@ -44,12 +43,19 @@ public class AroundImageEditText extends FrameLayout{
     }
 
     private void setAttribute(AttributeSet attrs){
+        tImageView = new ImageView(tContext);
 
         TypedArray typedArray = tContext.obtainStyledAttributes(attrs, R.styleable.AroundImageEditText);
-        firstColor = typedArray.getColor(R.styleable.AroundImageEditText_aietFirstImageBackground, DEFAULT_FIRST_BACKGROUND_COLOR);
-        secondColor = typedArray.getColor(R.styleable.AroundImageEditText_aietFirstImageBackground, DEFAULT_SECOND_BACKGROUND_COLOR);
+        firstBackGroundColor = typedArray.getColor(R.styleable.AroundImageEditText_aietFirstImageBackgroundColor, DEFAULT_FIRST_BACKGROUND_COLOR);
         firstImage = typedArray.getResourceId(R.styleable.AroundImageEditText_aietFirstImage, DEFAULT_FIRST_IMAGE_SOURCE);
+        secondBackgroundColor = typedArray.getColor(R.styleable.AroundImageEditText_aietFirstImageBackgroundColor, DEFAULT_SECOND_BACKGROUND_COLOR);
         secondImage = typedArray.getResourceId(R.styleable.AroundImageEditText_aietFirstImage, DEFAULT_SECOND_IMAGE_SOURCE);
+
+        tImageView.setImageResource(firstImage);
+        tImageView.setBackgroundColor(firstBackGroundColor);
+        tImageView.setBackgroundResource(R.drawable.default_first_background);
+
+        addView(tImageView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 
         typedArray.recycle();
     }
@@ -80,7 +86,7 @@ public class AroundImageEditText extends FrameLayout{
         objectAnimator.start();
     }
 
-    public void changeImage(final ImageView target, final int imgRes){
+    public void changeImage(final ImageView target, Boolean flag){
         final Animation anim_out = AnimationUtils.loadAnimation(tContext, android.R.anim.fade_out);
         final Animation anim_in  = AnimationUtils.loadAnimation(tContext, android.R.anim.fade_in);
         anim_out.setAnimationListener(new Animation.AnimationListener()
@@ -89,7 +95,9 @@ public class AroundImageEditText extends FrameLayout{
             @Override public void onAnimationRepeat(Animation animation) {}
             @Override public void onAnimationEnd(Animation animation)
             {
-                target.setImageResource(imgRes);
+                target.setImageResource(secondImage);
+                target.setBackgroundColor(secondBackgroundColor);
+                target.setBackgroundResource(R.drawable.default_second_background);
                 anim_in.setAnimationListener(new Animation.AnimationListener() {
                     @Override
                     public void onAnimationStart(Animation animation) {
@@ -106,24 +114,6 @@ public class AroundImageEditText extends FrameLayout{
         });
         target.startAnimation(anim_out);
     }
-
-    /*
-    private float[] getImageSize(ImageView imageView){
-        float[] floats = new float[1];
-
-        Rect rect = imageView.getDrawable().getBounds();
-        float scaleX = (float) imageView.getWidth() / (float) rect.width();
-        float scaleY = (float) imageView.getHeight() / (float) rect.height();
-        float scale = Math.min(scaleX, scaleY);
-        float width = scale * rect.width();
-        float height = scale * rect.height();
-
-        floats[0] = width;
-        floats[1] =height;
-
-        return floats;
-    }
-    */
 
     private void setEditText(EditText editText){
         tEditText = editText;
@@ -143,7 +133,6 @@ public class AroundImageEditText extends FrameLayout{
                 tEditText.getText().toString();
             }
         });
-
     }
 
     private void setImage(ImageView ImageView, int resId){
