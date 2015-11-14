d4668b8a33bd1ea224a9fdada38ac90699725535
tatsu73 <tatsuro.saito73@gmail.com>
2015-11-14 17:02:19 +0900

    17:00 commit

--- aroundimageedittextlibrary/src/main/java/com/tatsu73/aroundimageedittextlibrary/AroundImageEditText.java
+++ aroundimageedittextlibrary/src/main/java/com/tatsu73/aroundimageedittextlibrary/AroundImageEditText.java
@@ -1,5 +1,6 @@
 package com.tatsu73.aroundimageedittextlibrary;
 
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.content.Context;
@@ -26,6 +27,7 @@ public class AroundImageEditText extends FrameLayout{
     private EditText tEditText;
     private ImageView tImageView;
     private Boolean condition = false;
+    private Boolean imageCondition;
     private int firstBackGroundColor;
     private int secondBackgroundColor;
     private int firstImage;
@@ -50,7 +52,6 @@ public class AroundImageEditText extends FrameLayout{
         firstImage = typedArray.getResourceId(R.styleable.AroundImageEditText_aietFirstImage, DEFAULT_FIRST_IMAGE_SOURCE);
         secondBackgroundColor = typedArray.getColor(R.styleable.AroundImageEditText_aietFirstImageBackgroundColor, DEFAULT_SECOND_BACKGROUND_COLOR);
         secondImage = typedArray.getResourceId(R.styleable.AroundImageEditText_aietFirstImage, DEFAULT_SECOND_IMAGE_SOURCE);
-
         tImageView.setImageResource(firstImage);
         tImageView.setBackgroundColor(firstBackGroundColor);
         tImageView.setBackgroundResource(R.drawable.default_first_background);
@@ -85,31 +86,53 @@ public class AroundImageEditText extends FrameLayout{
     private void moveImage(){
         float toX = tEditText.getWidth();
         PropertyValuesHolder holderX;
-        if(condition){
+        if(!condition){
             holderX = PropertyValuesHolder.ofFloat("translationX", 0f, toX);
         } else {
             holderX = PropertyValuesHolder.ofFloat("translationX", 0f, -toX);
         }
 
-        PropertyValuesHolder holderAround = PropertyValuesHolder.ofFloat( "rotation", 0f, 360f );
+        PropertyValuesHolder holderAround = PropertyValuesHolder.ofFloat("rotation", 0f, 360f);
         ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(
-                tImageView, holderX,holderAround);
+                tImageView, holderX, holderAround);
 
-        objectAnimator.setDuration( 700 );
+        objectAnimator.setDuration(700);
+        objectAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (condition) {
+                    changeImage();
+                }
+                changeCondition();
+            }
+        });
         objectAnimator.start();
-        changeCondition();
     }
 
-    public void changeImage(Boolean flag){
+    public void changeImage(){
         final Animation anim_out = AnimationUtils.loadAnimation(tContext, android.R.anim.fade_out);
         final Animation anim_in  = AnimationUtils.loadAnimation(tContext, android.R.anim.fade_in);
         anim_out.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
             }
+
             @Override
             public void onAnimationRepeat(Animation animation) {
             }
+
             @Override
             public void onAnimationEnd(Animation animation) {
                 tImageView.setImageResource(secondImage);
@@ -119,9 +142,11 @@ public class AroundImageEditText extends FrameLayout{
                     @Override
                     public void onAnimationStart(Animation animation) {
                     }
+
                     @Override
                     public void onAnimationRepeat(Animation animation) {
                     }
+
                     @Override
                     public void onAnimationEnd(Animation animation) {
                     }
@@ -131,8 +156,7 @@ public class AroundImageEditText extends FrameLayout{
         });
         tImageView.startAnimation(anim_out);
     }
-
-    private void setEditText(EditText editText){
+    private void setEditText(final EditText editText){
         tEditText = editText;
         tEditText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -141,10 +165,9 @@ public class AroundImageEditText extends FrameLayout{
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
             }
-
             @Override
             public void afterTextChanged(Editable s) {
-                tEditText.getText().toString();
+                if(editText.getText())
             }
         });
 
@@ -154,6 +177,10 @@ public class AroundImageEditText extends FrameLayout{
 
             }
         });
+    }
+
+    public void setJudge(){
+        
     }
 
     private void changeCondition(){
