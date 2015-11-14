efc6339c3a5cd6e31df521757a5cc2e4ba6febbf
tatsu73 <tatsuro.saito73@gmail.com>
2015-11-14 23:39:43 +0900

    change java file(code review )

--- aroundimageedittextlibrary/src/main/java/com/tatsu73/aroundimageedittextlibrary/AroundImageEditText.java
+++ aroundimageedittextlibrary/src/main/java/com/tatsu73/aroundimageedittextlibrary/AroundImageEditText.java
@@ -23,58 +23,66 @@ import android.widget.ImageView;
  */
 public class AroundImageEditText extends FrameLayout{
 
-    private Context tContext;
-    private EditText tEditText;
-    private ImageView tImageView;
+    private static final int DEFAULT_FIRST_BACKGROUND_COLOR = Color.argb(255, 255, 0, 0);
+    private static final int DEFAULT_SECOND_BACKGROUND_COLOR = Color.argb(255, 0, 255, 0);
+    private static final int DEFAULT_FIRST_IMAGE_SOURCE = R.drawable.ic_close_grey_18dp;
+    private static final int DEFAULT_SECOND_IMAGE_SOURCE = R.drawable.ic_check_grey_18dp;
+    private Context Context;
+    private EditText EditText;
+    private ImageView ImageView;
     private Boolean condition = false;
     private Boolean imageCondition;
     private int firstBackGroundColor;
     private int secondBackgroundColor;
     private int firstImage;
     private int secondImage;
-    private static final int DEFAULT_FIRST_BACKGROUND_COLOR = Color.argb(255, 255, 0, 0);
-    private static final int DEFAULT_SECOND_BACKGROUND_COLOR = Color.argb(255, 0, 255, 0);
-    private static final int DEFAULT_FIRST_IMAGE_SOURCE = R.drawable.ic_close_grey_18dp;
-    private static final int DEFAULT_SECOND_IMAGE_SOURCE = R.drawable.ic_check_grey_18dp;
 
+    public AroundImageEditText(Context context){
+        super(context);
+    }
 
     public AroundImageEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
-        tContext = context;
+        Context = context;
+        setAttribute(attrs);
+    }
+    public AroundImageEditText(Context context,AttributeSet attrs,int defStyleAttr){
+        super(context, attrs, defStyleAttr);
+        Context = context;
         setAttribute(attrs);
     }
 
     private void setAttribute(AttributeSet attrs){
-        tImageView = new ImageView(tContext);
+        ImageView = new ImageView(Context);
 
-        TypedArray typedArray = tContext.obtainStyledAttributes(attrs, R.styleable.AroundImageEditText);
+        TypedArray typedArray = Context.obtainStyledAttributes(attrs, R.styleable.AroundImageEditText);
         firstBackGroundColor = typedArray.getColor(R.styleable.AroundImageEditText_aietFirstImageBackgroundColor, DEFAULT_FIRST_BACKGROUND_COLOR);
         firstImage = typedArray.getResourceId(R.styleable.AroundImageEditText_aietFirstImage, DEFAULT_FIRST_IMAGE_SOURCE);
         secondBackgroundColor = typedArray.getColor(R.styleable.AroundImageEditText_aietFirstImageBackgroundColor, DEFAULT_SECOND_BACKGROUND_COLOR);
         secondImage = typedArray.getResourceId(R.styleable.AroundImageEditText_aietFirstImage, DEFAULT_SECOND_IMAGE_SOURCE);
-        tImageView.setImageResource(firstImage);
-        tImageView.setBackgroundColor(firstBackGroundColor);
-        tImageView.setBackgroundResource(R.drawable.default_first_background);
+        ImageView.setImageResource(firstImage);
+        ImageView.setBackgroundColor(firstBackGroundColor);
+        ImageView.setBackgroundResource(R.drawable.default_first_background);
 
-        tImageView.setOnClickListener(new OnClickListener() {
+        ImageView.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 moveImage();
             }
         });
-        addView(tImageView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        addView(ImageView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         typedArray.recycle();
     }
 
     @Override
     public final void addView(View child, int index, ViewGroup.LayoutParams layoutParams) {
         if (child instanceof EditText) {
-            if (tEditText != null) {
+            if (EditText != null) {
                 throw new IllegalArgumentException("Can only have one EditText subview");
             }
             final LayoutParams lp = new LayoutParams(layoutParams);
             lp.gravity =Gravity.CENTER_VERTICAL;
-            lp.leftMargin = tImageView.getWidth()/2;
+            lp.leftMargin = ImageView.getWidth()/2;
             layoutParams = lp;
             setEditText((EditText) child);
         }
@@ -82,7 +90,7 @@ public class AroundImageEditText extends FrameLayout{
     }
 
     private void moveImage(){
-        float toX = tEditText.getWidth();
+        float toX = EditText.getWidth();
         PropertyValuesHolder holderX;
         if(!condition){
             holderX = PropertyValuesHolder.ofFloat("translationX", 0f, toX);
@@ -92,7 +100,7 @@ public class AroundImageEditText extends FrameLayout{
 
         PropertyValuesHolder holderAround = PropertyValuesHolder.ofFloat("rotation", 0f, 360f);
         ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(
-                tImageView, holderX, holderAround);
+                ImageView, holderX, holderAround);
         objectAnimator.setDuration(700);
         objectAnimator.addListener(new Animator.AnimatorListener() {
             @Override
@@ -116,8 +124,8 @@ public class AroundImageEditText extends FrameLayout{
     }
 
     public void changeImage(){
-        final Animation anim_out = AnimationUtils.loadAnimation(tContext, android.R.anim.fade_out);
-        final Animation anim_in  = AnimationUtils.loadAnimation(tContext, android.R.anim.fade_in);
+        final Animation anim_out = AnimationUtils.loadAnimation(Context, android.R.anim.fade_out);
+        final Animation anim_in  = AnimationUtils.loadAnimation(Context, android.R.anim.fade_in);
         anim_out.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -127,9 +135,9 @@ public class AroundImageEditText extends FrameLayout{
             }
             @Override
             public void onAnimationEnd(Animation animation) {
-                tImageView.setImageResource(secondImage);
-                tImageView.setBackgroundColor(secondBackgroundColor);
-                tImageView.setBackgroundResource(R.drawable.default_second_background);
+                ImageView.setImageResource(secondImage);
+                ImageView.setBackgroundColor(secondBackgroundColor);
+                ImageView.setBackgroundResource(R.drawable.default_second_background);
                 anim_in.setAnimationListener(new Animation.AnimationListener() {
                     @Override
                     public void onAnimationStart(Animation animation) {
@@ -143,15 +151,15 @@ public class AroundImageEditText extends FrameLayout{
                     public void onAnimationEnd(Animation animation) {
                     }
                 });
-                tImageView.startAnimation(anim_in);
+                ImageView.startAnimation(anim_in);
             }
         });
-        tImageView.startAnimation(anim_out);
+        ImageView.startAnimation(anim_out);
     }
 
     private void setEditText(final EditText editText){
-        tEditText = editText;
-        tEditText.addTextChangedListener(new TextWatcher() {
+        EditText = editText;
+        EditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
@@ -168,7 +176,7 @@ public class AroundImageEditText extends FrameLayout{
             }
         });
 
-        tEditText.setOnFocusChangeListener(new OnFocusChangeListener() {
+        EditText.setOnFocusChangeListener(new OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 onFocusChanged();
