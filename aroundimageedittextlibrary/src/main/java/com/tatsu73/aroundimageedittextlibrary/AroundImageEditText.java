c75e2d0b4caabca548c2001c6ff756c5e7d3124f
tatsu73 <tatsuro.saito73@gmail.com>
2015-11-14 12:12:27 +0900

    create java file

---
+++ aroundimageedittextlibrary/src/main/java/com/tatsu73/aroundimageedittextlibrary/AroundImageEditText.java
@@ -0,0 +1,24 @@
+package com.tatsu73.aroundimageedittextlibrary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+/**
+ * Created by saito.tatsuro on 2015/11/14.
+ */
+public class AroundImageEditText extends FrameLayout{
+
+    Context context;
+    EditText editText;
+    ImageView imageView;
+
+    public AroundImageEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+
+}
