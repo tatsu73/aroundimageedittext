20014b72718f7b16d380fd57a6c1bb33b1d46ac1
tatsu73 <tatsuro.saito73@gmail.com>
2015-11-14 12:31:53 +0900

    add method

--- aroundimageedittextlibrary/src/main/java/com/tatsu73/aroundimageedittextlibrary/AroundImageEditText.java
+++ aroundimageedittextlibrary/src/main/java/com/tatsu73/aroundimageedittextlibrary/AroundImageEditText.java
@@ -1,7 +1,9 @@
 package com.tatsu73.aroundimageedittextlibrary;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -17,8 +19,25 @@ public class AroundImageEditText extends FrameLayout{
 
     public AroundImageEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
+        setAttribute(attrs);
+    }
+
+    private void setAttribute(AttributeSet attrs){
+        final TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.AroundImageEditText);
+
+        final int defaultPadding = (int) TypedValue.applyDimension();
+        final int paddingLeft = typedArray.getDimensionPixelSize(R.styleable.AroundImageEditText_aietPaddingLeft, defaultPadding);
+        final int paddingTop = typedArray.getDimensionPixelSize(R.styleable.AroundImageEditText_aietPaddingTop, 0);
+        final int paddingRight = typedArray.getDimensionPixelSize(R.styleable.AroundImageEditText_aietPaddingRight, 0);
+        final int paddingBottom = typedArray.getDimensionPixelSize(R.styleable.AroundImageEditText_aietPaddingBottom, 0);
+
 
     }
+
+    
+
+
 
 
 }
